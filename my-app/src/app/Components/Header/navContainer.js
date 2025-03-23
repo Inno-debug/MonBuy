3e34ab0f7615279fb724d5922865f2094f4d0bb9
Inno <enoatoz6@gmail.com>
2025-03-23 19:33:02 -0400

    adding base elements

---
+++ my-app/src/app/Components/Header/navContainer.js
@@ -0,0 +1,9 @@
+export default function NavContainer() {
+  return (
+    <div className="grid grid-cols-3">
+      <div className="bg-amber-50">Hello</div>
+      <div className="bg-amber-300">hi</div>
+      <div className="bg-amber-900">dad</div>
+    </div>
+  );
+}
